5842c952a8b38e1b6ac88030431b8a187b6cf9fa
Mahdi Nasser <99844788+mahdiinasser@users.noreply.github.com>
2022-02-21 18:41:26 +0200

    Add files via upload

---
+++ COE_211/StaffManagement/Employee.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Employee{
+	
+	
+
+	String firstName;
+	String lastName;
+	int age;
+	double monthlysal;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public double getMonthlySal() {
+		return monthlysal;
+	}
+	
+	
+
+
+
+
+
+
+
+
+}
