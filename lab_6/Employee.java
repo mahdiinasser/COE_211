98c631fb39403b1cb1625f22ab454566ece11eb3
Mahdi Nasser <99844788+mahdiinasser@users.noreply.github.com>
2022-03-14 16:54:38 +0200

    Add files via upload

--- lab_6/Employee.java
+++ lab_6/Employee.java
@@ -1,23 +1,29 @@
 import java.util.Scanner;
 
 public class Employee {
-    // Initializing variables
+ 
     private String firstName;
     private String lastName;
     private int age;
     private double salary;
 
     public Employee() {
-        // Get user input
+        
         Scanner scan = new Scanner(System.in);
-
-        /**
-         * Continue code here to ask for the first name, last name, age, and salary, IN THAT ORDER
-         */
+        System.out.println("Enter your first name: ");
+        firstName = scan.nextLine();
+        System.out.println("Enter your last name: ");
+        lastName = scan.nextLine();
+        System.out.print("Enter your age: ");
+        age = scan.nextInt();
+        System.out.print("Enter your salary: ");
+        salary = scan.nextDouble();
 
     }
 
     public String toString() {
-        // insert logic for toString method
+        return  "Employee information: "+firstName+" "+lastName+", "+age+", "+salary+".";
     }
+
 }
+		
--- lab_6/EntryPoint.java
+++ lab_6/EntryPoint.java
@@ -2,31 +2,45 @@ import java.util.Scanner;
 
 public class EntryPoint {
     public static void main (String[] args) {
-	Scanner scan = new Scanner(System.in);
-	int service;
-	
-	AdvancedWeek advw = new AdvancedWeek();
-	BasicWeek basw = new BasicWeek();
-	
-	do {
-	System.out.println("Which service would you like to choose?");
-	service = scan.nextInt();
-	} while (service < 1 || service > 4);
-	
-	
-	switch (service) {
-	case 1:
-		basw.printDays();
-		break;
-	case 2:
-		advw.printDays();
-		break;
-	case 3:
-		Calculator calc = new Calculator();
-		break;
-	default:
-		System.out.println("Not passing!");
-	}
-	
+        Scanner scan = new Scanner(System.in);
+        boolean proceed = true;
+        while(proceed){
+            System.out.print(
+                "Which service would you like to use? \n"
+                +"[1]: Basic week visualiser \n"
+                +"[2]: Advanced week visualizer \n"
+                +"[3]: Basic calculator \n"
+                +"[4]: Employee repertoire \n"
+                );
+            int service = scan.nextInt();
+            if(service <1 || service > 4) {
+                System.out.println("Please make sure you pick a number between 1 and 4: ");
+            }else{
+                switch (service) {
+                    case 1:
+                        BasicWeek base=new BasicWeek();
+                        base.printDays();
+                        break;
+                    case 2:
+                        AdvancedWeek advanced=new AdvancedWeek();
+                        advanced.printDays();
+                        break;
+                    case 3:
+                        Calculator calc=new Calculator();
+                        break;
+                    case 4:
+                        Employee employee=new Employee();
+                        System.out.println(employee);
+                        break;
+                    
+                }  
+                System.out.println("Would you like to perform another operation? (y/n) ");
+                scan.nextLine();
+                proceed =scan.nextLine().trim().toLowerCase().equals("y");             
+            }   
+                        
+            
+        }      
+
     }
 }
